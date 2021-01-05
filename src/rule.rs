a24001e054cf3139dea8694c31b5593e12e67eb7
mcdobr <mircea.dobreanu@gmail.com>
2021-01-05 23:22:50 +0200

    Create dummy rule struct

--- src/rule.rs
+++ src/rule.rs
@@ -1,14 +1,5 @@
-use hyper::{Request, Body};
-
-#[derive(Clone)]
 pub struct Rule {
     variables: String,
     operator: String,
     actions: String
-}
-
-impl Rule {
-    pub(crate) fn matches(&self, request: &Request<Body>) -> bool {
-        unimplemented!()
-    }
 }
