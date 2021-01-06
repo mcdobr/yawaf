d122f89f9b2290afe154925a191d7f324ce4a09a
mcdobr <mircea.dobreanu@gmail.com>
2021-01-07 00:13:34 +0200

    Write basic logic for checking request against the WAF rules without actually having said rules

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod rule;
 mod reverse_proxy;
+mod waf_running_mode;
 
 use hyper::service::{make_service_fn, service_fn};
 use std::convert::Infallible;
@@ -7,6 +8,7 @@ use hyper::{Server, Client};
 use std::net::SocketAddr;
 use hyper::client::HttpConnector;
 use crate::reverse_proxy::ReverseProxy;
+use crate::waf_running_mode::WafRunningMode::DetectionOnly;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -28,6 +30,9 @@ hosts:
         client: http_client,
         scheme: "http".to_owned(),
         authority: "example.com".to_owned(),
+        // WAF specific properties
+        rules: vec![],
+        running_mode: DetectionOnly
     });
 
     let address = SocketAddr::from(([127, 0, 0, 1], 3030));
--- src/rule.rs
+++ src/rule.rs
@@ -1,5 +1,14 @@
+use hyper::{Request, Body};
+
+#[derive(Clone)]
 pub struct Rule {
     variables: String,
     operator: String,
     actions: String
+}
+
+impl Rule {
+    pub(crate) fn matches(&self, request: &Request<Body>) -> bool {
+        unimplemented!()
+    }
 }
