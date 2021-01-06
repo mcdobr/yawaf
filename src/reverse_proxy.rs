c770f52a21908911d802a16186dc37adb054ac38
mcdobr <mircea.dobreanu@gmail.com>
2021-01-06 23:22:44 +0200

    Split handle function

--- src/reverse_proxy.rs
+++ src/reverse_proxy.rs
@@ -1,25 +1,44 @@
-use hyper::{Client, Request, Body, Response, Error, HeaderMap};
+use hyper::{Client, Request, Body, Response, Error, HeaderMap, Uri};
 use hyper::client::HttpConnector;
+use hyper::http::HeaderValue;
 
 pub(crate) struct ReverseProxy {
     pub(crate) scheme: String,
     pub(crate) authority: String,
-    pub(crate) client: Client<HttpConnector>
+    pub(crate) client: Client<HttpConnector>,
 }
 
 impl ReverseProxy {
     pub(crate) async fn handle_request(&self, mut request: Request<Body>) -> Result<Response<Body>, Error> {
+        *request.uri_mut() = self.rewrite_uri(&request);
+        *request.headers_mut() = self.build_header_map(&request);
+
+
+        log::debug!("Request == {:?}", request);
+        let response = self.client.request(request).await.unwrap();
+        log::debug!("Response == {:?}", response);
+        return Ok(response);
+    }
+
+    fn rewrite_uri(&self, request: &Request<Body>) -> Uri {
         // Change the request's URI
         let mut uri_builder = hyper::Uri::builder()
             .scheme(&*self.scheme)
             .authority(&*self.authority);
+        // Copy path and query params
+        if let Some(path_and_query) = request.uri().path_and_query() {
+            uri_builder = uri_builder.path_and_query(path_and_query.clone());
+        }
+        return uri_builder.build().unwrap();
+    }
 
-
+    fn build_header_map(&self, request: &Request<Body>) -> HeaderMap<HeaderValue> {
         // Remove headers not whitelisted
-        const ALLOWED_HEADERS: [&str; 3] = [
+        const ALLOWED_HEADERS: [&str; 4] = [
             "accept",
             "user-agent",
             "DNT",
+            "X-Forwarded-For"
             // hyper::header::HOST.as_str()
             // hyper::header::ACCEPT.as_str().clone(),
             // hyper::header::USER_AGENT.as_str().clone()
@@ -31,19 +50,15 @@ impl ReverseProxy {
                 filtered_headers.insert(header_name, header_value.clone());
             }
         }
-        // filtered_headers.insert("host", request.uri().authority())
-        *request.headers_mut() = filtered_headers;
+
+        // if filtered_headers.contains_key("X-Forwarded-For") {
+        //     let xForwardedFor = filtered_headers.get_mut("X-Forwarded-For");
+        //
+        // } else {
+        //     request.get
+        // }
 
 
-        // Copy path and query params
-        if let Some(path_and_query) = request.uri().path_and_query() {
-            uri_builder = uri_builder.path_and_query(path_and_query.clone());
-        }
-        *request.uri_mut() = uri_builder.build().unwrap();
-
-        log::debug!("Request == {:?}", request);
-        let response = self.client.request(request).await.unwrap();
-        log::debug!("Response == {:?}", response);
-        return Ok(response);
+        return filtered_headers;
     }
 }
