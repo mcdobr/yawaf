6212ce9b7c4224748306662af2db53e55a42b564
Mircea Dobreanu <mircea.dobreanu@gmail.com>
2021-03-18 21:54:24 +0000

    Feature/break reverse proxy data structure (#2)
    * Move reverse proxy logic to its own file
    
    * Create dummy rule struct
    
    * Split handle function
    
    * Write basic logic for checking request against the WAF rules without actually having said rules

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,57 @@
+mod rule;
+mod reverse_proxy;
+mod waf_running_mode;
+
 use hyper::service::{make_service_fn, service_fn};
 use std::convert::Infallible;
-use hyper::{Request, Body, Response, Server, Client, HeaderMap};
+use hyper::{Server, Client};
 use std::net::SocketAddr;
 use hyper::client::HttpConnector;
+use crate::reverse_proxy::ReverseProxy;
+use crate::waf_running_mode::WafRunningMode::DetectionOnly;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
 
-
-
-    let _config : &str =
-    "
+    let _config: &str =
+        "
 hosts:
     - example.com
     - 127.0.0.1:10000
 
     ";
 
-    let address = SocketAddr::from(([127, 0, 0, 1], 3030));
 
-    let service = make_service_fn(|_| async {
-        return Ok::<_, Infallible>(service_fn(handle_request));
-    });
-
-    let server = Server::bind(&address).serve(service);
-
-
-    server.await?;
-
-    return Ok(());
-}
-
-struct ReverseProxy {
-    scheme: String,
-    authority: String,
-    client: Client<HttpConnector<hyper::client::HttpConnector>>
-}
-
-
-// todo: change unwraps() to log and exit
-async fn handle_request(mut request: Request<Body>) -> Result<Response<Body>, Infallible> {
-    // Change the request's URI
-    let mut uri_builder = hyper::Uri::builder()
-        .scheme("http")
-        .authority("example.com");
-    *request.uri_mut() = uri_builder.build().unwrap();
-
-
-    // Remove headers not whitelisted
-    const ALLOWED_HEADERS: [&str; 3] = [
-        "accept",
-        "user-agent",
-        "DNT",
-        // hyper::header::HOST.as_str()
-        // hyper::header::ACCEPT.as_str().clone(),
-        // hyper::header::USER_AGENT.as_str().clone()
-    ];
-
-    let mut filtered_headers = HeaderMap::new();
-    for (header_name, header_value) in request.headers() {
-        if ALLOWED_HEADERS.contains(&header_name.as_str()) {
-            filtered_headers.insert(header_name, header_value.clone());
-        }
-    }
-    // filtered_headers.insert("host", request.uri().authority())
-    *request.headers_mut() = filtered_headers;
-
-
-    // Copy path and query params
-    if let Some(path_and_query) = request.uri().path_and_query() {
-        uri_builder = uri_builder.path_and_query(path_and_query.clone());
-    }
-
-
-    log::debug!("Request == {:?}", request);
-
-    // todo: change ownership such that we don't create a client for every request, but inject it somehow
     let http_connector = HttpConnector::new();
     let http_client = Client::builder().build(http_connector);
-    let response = http_client.request(request).await.unwrap();
-    log::debug!("Response == {:?}", response);
-    return Ok(response);
+
+    let reverse_proxy = std::sync::Arc::new(ReverseProxy {
+        client: http_client,
+        scheme: "http".to_owned(),
+        authority: "example.com".to_owned(),
+        // WAF specific properties
+        rules: vec![],
+        running_mode: DetectionOnly
+    });
+
+    let address = SocketAddr::from(([127, 0, 0, 1], 3030));
+
+    let service = make_service_fn(move |_connection| {
+        let reverse_proxy_service_ref = reverse_proxy.clone();
+        async {
+            return Ok::<_, Infallible>(service_fn(move |request| {
+                let reverse_proxy_request_ref = reverse_proxy_service_ref.clone();
+                async move {
+                    reverse_proxy_request_ref.handle_request(request).await
+                }
+            }));
+        }
+    });
+
+
+    let server = Server::bind(&address)
+        .serve(service);
+    server.await?;
+    return Ok(());
 }
