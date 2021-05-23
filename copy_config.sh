d1b5dece91dd14bc2bef5c3ae8d218681d5852c2
mcdobr <mircea.dobreanu@gmail.com>
2021-05-23 18:49:02 +0300

    Fix docker config

--- copy_config.sh
+++ copy_config.sh
@@ -6,3 +6,4 @@ if cargo "$@"; then
     [ -d "$DIR/target/debug" ] && cp -r "$DIR/config" "$DIR/target/debug/config"
     [ -d "$DIR/target/release" ] && cp -r "$DIR/config" "$DIR/target/release/config"
 fi
+
